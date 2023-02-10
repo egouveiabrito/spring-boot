10fde5161c8947357aff8feccfa514354c611760
Edson Gouveia de Brito <edsongouveiabrito@gmail.com>
2023-02-10 19:26:06 -0300

    Implementação do rabbitMq para estudo - parte 2

---
+++ src/main/java/com/api/usersapi/configuration/RabbitMQConfig.java
@@ -0,0 +1,52 @@
+package com.api.usersapi.configuration;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+
+@Configuration
+public class RabbitMQConfig {
+
+        public static final String USER_SAVE_QUEUE = "save.user.queue";
+        public static final String USER_SAVE_EXCHANGE = "user.save.exchange";
+        public static final String USER_SAVE_ROUTING_KEY = "save.user.key";
+
+
+        public static final String USER_SAVE_QUEUE_DEADLETTER = "save.user.queue.deadLetter";
+        public static final String USER_SAVE_EXCHANGE_DEADLETTER = "user.save.exchange.deadLetter";
+        public static final String USER_SAVE_ROUTING_KEY_DEADLETTER = "save.user.key.deadLetter";
+
+        @Bean
+        Queue queue() { return QueueBuilder.durable(USER_SAVE_QUEUE).withArgument("x-dead-letter-exchange", USER_SAVE_EXCHANGE_DEADLETTER).withArgument("x-dead-letter-routing-key", USER_SAVE_ROUTING_KEY_DEADLETTER).build();
+        }
+
+        @Bean
+        DirectExchange exchange() { return new DirectExchange(USER_SAVE_EXCHANGE); }
+        @Bean
+        Binding binding() { return BindingBuilder.bind(queue()).to(exchange()).with(USER_SAVE_ROUTING_KEY); }
+
+        @Bean
+        DirectExchange exchangeDeadLetter() {
+            return new DirectExchange(USER_SAVE_EXCHANGE_DEADLETTER);
+        }
+
+        @Bean
+        Queue queueDeadLetter() { return QueueBuilder.durable(USER_SAVE_QUEUE_DEADLETTER).build(); }
+
+        @Bean
+        Binding bindingDeadLetter() { return BindingBuilder.bind(queueDeadLetter()).to(exchangeDeadLetter()).with(USER_SAVE_ROUTING_KEY_DEADLETTER); }
+
+        @Bean
+        public MessageConverter jsonMessageConverter() { return new Jackson2JsonMessageConverter();  }
+
+        public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+            final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+            rabbitTemplate.setMessageConverter(jsonMessageConverter());
+            return rabbitTemplate;
+        }
+    }
